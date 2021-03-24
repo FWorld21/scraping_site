98e8442e619209b2f7d66bfad0cb3f340588cd98
FWorld <FWorld21@protonmail.com>
2021-03-24 13:43:19 +0500

    russian lang in admin page

--- scraping/models.py
+++ scraping/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=30, verbose_name="Название города")
+    slug = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
