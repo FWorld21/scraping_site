a2cc94739e6428f18c1e4a395b0d10edfd05c6f7
FWorld <FWorld21@protonmail.com>
2021-03-24 17:29:41 +0500

    auto create slug and auto translate ru to eng

--- scraping/models.py
+++ scraping/models.py
@@ -1,14 +1,32 @@
 from django.db import models
+from . ru_to_eng import translate
 
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=30, verbose_name="Название города")
+    name = models.CharField(max_length=30, verbose_name="Название города", unique=True)
     slug = models.CharField(max_length=30, blank=True)
 
     class Meta:
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = translate(self.name)
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Язык программирования", unique=True)
+    slug = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
 
     def __str__(self):
         return self.name
