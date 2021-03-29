088cdb3fe2b063c5ff0ecb6375308946c5c4a3dc
FWorld <FWorld21@protonmail.com>
2021-03-29 13:54:56 +0500

    Created 2 tested vacancies and 2 choices for users in site

--- scraping/views.py
+++ scraping/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from . models import Vacancy
-
+from . forms import FormForSearch
 
 # Create your views here.
 def home(request):
-    vacancies = Vacancy.objects.all()
-    return render(request, "scraping/home.html", {"vacancies":vacancies})
+    form = FormForSearch()
+
+    city = request.GET.get("city")
+    lang = request.GET.get("lang")
+    vacancies = []
+    if city or lang:
+        _filter = {}
+        if city:
+            _filter["city__slug"] = city
+        if lang:
+            _filter["language__slug"] = lang
+        vacancies = Vacancy.objects.filter(**_filter)
+
+    return render(request, "scraping/home.html", {"vacancies":vacancies, "form": form})
